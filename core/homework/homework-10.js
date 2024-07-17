4b7b680bff4d92ef28ea935896335f3fcc429f4e
Alex <alexandr@192.168.1.2>
2024-07-17 18:23:35 +0300

    hm-10

--- core/homework/homework-10.js
+++ core/homework/homework-10.js
@@ -1,7 +1,7 @@
 class Pointer {
-    constructor(start, length, buffer) {
-        this.start = start;
-        this.length = length;
+    constructor(index, size, buffer) {
+        this.index = index;
+        this.size = size;
         this.buffer = buffer;
     }
 
@@ -9,40 +9,40 @@ class Pointer {
         const bufferArr = new Uint8Array(newBuffer);
         const arr = new Uint8Array(this.buffer);
 
-        if (this.length !== bufferArr.byteLength) {
+        if (this.size !== bufferArr.byteLength) {
             throw new Error('New data size and current data size are different');
         }
 
-        for (let i = this.start; i < this.start + this.length; i += 1) {
+        for (let i = this.index; i < this.index + this.size; i += 1) {
             arr[i] = bufferArr[i];
         }
     }
 }
 
 class StackPointer extends Pointer {
-    constructor(start, length, buffer) {
-        super(start, length, buffer);
+    constructor(index, size, buffer) {
+        super(index, size, buffer);
     }
 
     deref() {
-        return new Uint8Array(this.buffer, this.start, this.length).buffer;
+        return new Uint8Array(this.buffer, this.index, this.size).buffer;
     }
 }
 
 class HeapPointer extends Pointer {
-    constructor(start, length, buffer, heap) {
-        super(start, length, buffer);
+    constructor(index, size, buffer, heap) {
+        super(index, size, buffer);
         this.heap = heap;
     }
 
     free() {
         const arr = new Uint8Array(this.buffer);
 
-        for (let i = this.start; i < this.start + this.length; i += 1) {
+        for (let i = this.start; i < this.index + this.size; i += 1) {
             arr[i] = 0;
         }
 
-        this.heap.heapOffset -= this.length;
+        this.heap.heapPointers = this.heap.heapPointers.filter(p => p.index !== index);
     }
 }
 
@@ -51,29 +51,29 @@ class Memory {
         this.buffer = new ArrayBuffer(size);
         this.stackSize = stackData.stack;
         this.heapSize = size - this.stackSize;
-        this.stackOffset = 0;
-        this.heapOffset = this.stackSize;
-        this.lastPointerInStack = null;
+        this.stackPointers = [];
+        this.heapPointers = [];
     }
 
     // Stack methods
-    push(buffer) {
-        this.checkStackSize(buffer);
+    push(buffer) {        
+        const len = bufferArr.byteLength;
+        const lastPointer = this.stackPointers[this.stackPointers.length - 1];
+        const start = lastPointer
+            ? lastPointer.index + lastPointer.size
+            : 0
+        
+        this.checkStackSize(start, buffer);
 
         const arr = new Uint8Array(this.buffer);
         const bufferArr = new Uint8Array(buffer);
 
-        let len = 0;
-
-        for (let i = this.stackOffset; i < this.stackOffset + bufferArr.byteLength; i += 1) {
+        for (let i = start; i < start + len; i += 1) {
             arr[i] = bufferArr[i];
-            len += 1;
         }
 
-        const start = this.stackOffset;
-        this.stackOffset += len;
         const pointer = new StackPointer(start, len, this.buffer);
-        this.lastPointerInStack = pointer;
+        this.stackPointers.push(pointer);
 
         return pointer;
     }
@@ -82,48 +82,69 @@ class Memory {
         this.checkIfStackIsEmpty();
 
         const arr = new Uint8Array(this.buffer);
+        const lastPointer = this.stackPointers.pop();
+        const {index, size} = lastPointer;
 
-        for (let i = this.stackOffset - 1; i > this.stackOffset - 1 - this.lastPointerInStack.length; i -= 1) {
+        for (let i = index; i < index + size; i += 1) {
             arr[i] = 0;
         }
-
-        this.stackOffset -= this.lastPointerInStack.length;
     }
 
     // Heap methods
     alloc(bytes) {
-        this.checkHeapSize(bytes);
+        let start;
+        let pointerToInsert;
+
+        if (this.heapPointers.length === 0) {
+            this.checkHeapSize(this.stackSize, bytes);
+            start = 0;
+            pointerToInsert = 1;
+        }
+
+        for (let i = 0; i < this.heapPointers.length - 1; i += 1) {
+            const {index, size} = this.heapPointers[i];
+            const {index: nextIndex} = this.heapPointers[i + 1];
+
+            if (nextIndex - (index + size) >= bytes) {
+                start = index + size;
+                pointerToInsert = i + 1;
+                break;
+            }
+        }
+
+        if (start === undefined) {
+            start = this.heapPointers[this.heapPointers.length - 1].index + this.heapPointers[this.heapPointers.length - 1].size;
+            this.checkHeapSize(start, bytes);
+            pointerToInsert = this.heapPointers.length;
+        }
 
         const arr = new Uint8Array(this.buffer);
 
-        let len = 0;
-
-        for (let i = this.heapOffset; i < this.heapOffset + bytes; i += 1) {
+        for (let i = start; i < start + bytes; i += 1) {
             arr[i] = 1;
-            len += 1;
         }
 
-        const start = this.heapOffset;
-        this.heapOffset += len;
+        const pointer = new HeapPointer(start, bytes, this.buffer, this);
+        this.heapPointers.splice(pointerToInsert, 0, pointer);
 
-        return new HeapPointer(start, len, this.buffer, this);
+        return pointer;
     }
     
     // Help methods
-    checkStackSize(buffer) {
-        if (this.stackOffset + buffer.byteLength >= this.stackSize) {
+    checkStackSize(offset, buffer) {
+        if (offset + buffer.byteLength > this.stackSize) {
             throw new RangeError('Stack is overloaded');
         }
     }
 
-    checkHeapSize(bytes) {
-        if (this.heapOffset + bytes >= this.heapSize) {
+    checkHeapSize(offset, bytes) {
+        if (offset + bytes > this.heapSize) {
             throw new RangeError('Heap is overloaded');
         }
     }
 
     checkIfStackIsEmpty() {
-        if (this.stackOffset.length === 0) {
+        if (this.stackPointers.length === 0) {
             throw new Error('Stack is empty');
         };
     }
