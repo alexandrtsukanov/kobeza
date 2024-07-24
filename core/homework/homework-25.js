96bfe1e75d8ce49980f2b665d61bf334cfcdad5d
Alex <alexandr@192.168.1.2>
2024-07-24 21:56:58 +0300

    hm-25

--- core/homework/homework-25.js
+++ core/homework/homework-25.js
@@ -38,7 +38,7 @@ function throttle(cb, ms) {
 
         setTimeout(() => {
             isThrottled = false;
-            
+
             if (args && context) {
                 inner.apply(args, context);
                 args = undefined;
@@ -56,8 +56,145 @@ function laugh2() {
   
 const throttledLaugh = throttle(laugh2, 3000);
   
-throttledLaugh(); // Выполнится сразу
-throttledLaugh();
-throttledLaugh();
-throttledLaugh();
-throttledLaugh(); // Выполнится через 300 мс
+// throttledLaugh(); // Выполнится сразу
+// throttledLaugh();
+// throttledLaugh();
+// throttledLaugh();
+// throttledLaugh(); // Выполнится через 300 мс
+
+class EventEmitter {
+    constructor() {
+        this.callbacks = new Map();
+    };
+
+    on(event, cb) {
+        if (!this.callbacks.has(event)) {
+            this.callbacks.set(event, new Set());
+        }
+        
+        this.callbacks.get(event).add(cb);
+    };
+
+    off(event, cb) {
+        if (!this.callbacks.has(event)) {
+            return;
+        }
+
+        if (cb) {
+            this.callbacks.get(event).delete(cb);
+        } else {
+            this.callbacks.get(event).clear();
+        }
+    };
+
+    emit(event, ...args) {
+        if (!this.callbacks.has(event)) {
+            return;
+        }
+        
+        this.callbacks.get(event).forEach(cb => cb(...args));
+    };
+
+    once(event, cb) {
+        if (!this.callbacks.has(event)) {
+            return;
+        }
+
+        const callbacks = this.callbacks.get(event);
+
+        const toCall = (...args) => {
+            cb(...args)
+        };
+
+        toCall();
+
+        if (callbacks.has(cb)) {
+            this.off(event, cb);
+        };
+    };
+}
+
+const ee = new EventEmitter();
+
+// const f1 = () => {console.log(1)}
+// const f2 = () => {console.log(2)}
+// const f3 = () => {console.log(3)}
+// const f4 = () => {console.log(4)}
+// const f5 = () => {console.log(5)}
+
+// ee.on('foo', f1);
+// ee.on('foo', f2);
+// ee.on('bar', f3);
+// ee.on('bar', f4);
+// ee.on('bar', f5);
+
+// ee.off('bar', f5);
+// ee.off('bar', f3);
+
+// ee.emit('foo');
+// ee.emit('bar');
+
+// ee.once('foo', f2);
+// ee.emit('foo');
+
+function waterfall(iterable, finalCb) {
+    let error = null;
+    let prevArgs = [];
+
+    const iterator = iterable[Symbol.iterator]();
+
+    const innerCb = (err, ...args) => {
+        if (err) {
+            finalCb(err);
+            error = err;
+            return;
+        }
+
+        prevArgs = args;
+    }
+
+    while (true) {
+        if (error) return;
+
+        const currentState = iterator.next();
+        
+        if (currentState.done) {
+            finalCb(null, ...prevArgs);
+            return;
+        }
+
+        currentState.value(...prevArgs, innerCb);
+    }
+}
+
+// waterfall([
+//     (cb) => {
+//         cb(null, 'one', 'two');
+//     },
+  
+//     (arg1, arg2, cb) => {
+//         console.log(arg1); // one
+//         console.log(arg2); // two
+//         cb(null, 'three');
+//     },
+  
+//     (arg1, cb) => {
+//         console.log(arg1); // three
+//         cb(null, 'done');
+//     }
+// ], (err, result) => {
+//     console.log(result); // done
+// });
+  
+// waterfall(new Set([
+//     (cb) => {
+//         cb('ha-ha!');
+//     },
+  
+//     (arg1, cb) => {
+//         cb(null, 'done');
+//     }
+// ]), (err, result) => {
+//     console.log(err);    // ha-ha!
+//     console.log(result); // undefined
+// });
